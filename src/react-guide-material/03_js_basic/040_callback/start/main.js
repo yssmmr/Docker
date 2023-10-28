2bbb095c6eeddee07c5d0018ccbe90145c2ececd
yssmmr <noppie429@gmail.com>
2023-10-28 22:12:27 +0900

    UPDATE callback

--- src/react-guide-material/03_js_basic/040_callback/start/main.js
+++ src/react-guide-material/03_js_basic/040_callback/start/main.js
@@ -1,15 +1,10 @@
+function print(fn) {
+  const result = fn(2);
+  console.log(result);
+}
+
 function fn(number) {
-    return number * 2;
-  }
-  
-  console.log(fn(2));
-  console.log(fn(4));
-  
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
-  
-  const fnArrowObj = number => ({ result: number * 2 })
-  
-  console.log(fnArrowObj(2));
+  return number * 2;
+}
+
+print(fn);
