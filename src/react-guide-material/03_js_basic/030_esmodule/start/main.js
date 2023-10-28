152a21215ac39d195887c4879e521293a0e3d1ac
yssmmr <noppie429@gmail.com>
2023-10-28 17:47:27 +0900

    UPDATE アロー関数

---
+++ src/react-guide-material/03_js_basic/030_esmodule/start/main.js
@@ -0,0 +1,20 @@
+function fn(number) {
+  return number * 2;
+}
+console.log(fn(2));
+
+/* アロー関数
+  const fnArrow = (number) => {
+  return number * 2;
+}
+*/
+
+/* 引数が一つの場合は（）が、処理が一行の場合は｛｝が省略できる */
+const fnArrow = number => number * 2
+
+/* 戻り値がオブジェクトの場合、処理を()で囲う */
+
+const fnArrowObj = number => ({ result: number * 2})
+
+console.log(fnArrowObj(2));
+
