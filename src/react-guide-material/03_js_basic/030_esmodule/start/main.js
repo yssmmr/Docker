0308ae5ccceac08a1859cd1aa0d13f98fbc809f4
yssmmr <noppie429@gmail.com>
2023-10-28 21:59:27 +0900

    UPDATE esmodule

--- src/react-guide-material/03_js_basic/030_esmodule/start/main.js
+++ src/react-guide-material/03_js_basic/030_esmodule/start/main.js
@@ -1,20 +1,6 @@
-function fn(number) {
-  return number * 2;
-}
-console.log(fn(2));
+import funcB, { hello, User } from "./module.js"
+/* defaltをimportする場合には、｛｝がいらない */
 
-/* アロー関数
-  const fnArrow = (number) => {
-  return number * 2;
-}
-*/
-
-/* 引数が一つの場合は（）が、処理が一行の場合は｛｝が省略できる */
-const fnArrow = number => number * 2
-
-/* 戻り値がオブジェクトの場合、処理を()で囲う */
-
-const fnArrowObj = number => ({ result: number * 2})
-
-console.log(fnArrowObj(2));
-
+hello();
+const user = new User("Tom");
+user.hello();
